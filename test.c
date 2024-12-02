8223ae0b35d2afd5ec885f2692095d9b555fe882
cameraDD <kevin1511249859@outlook.com>
2024-12-02 17:14:00 +0800

    test

--- test.c
+++ test.c
@@ -14,13 +14,11 @@ int combination(int n, int k) {
 	return C[k];  
 }  
 int main(void){
-	int n,sum=0;
-	scanf("%d",&n);
-	for(int i=0;i<=n-i;i+=1){
-		sum+=combination(n-i,i);
+
+
 		
-	}
-	printf("%d",sum);
+	
+	printf("%d");
 	return 0;
 	
 	
